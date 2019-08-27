905df25eb76bcdec8d1e919cb781fcaf82dc2964
Jakub Holy <jakub.holy@telia.no>
2019-08-27 21:38:17 +0200

    v0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-concordion "0.1.1"
+(defproject clj-concordion "0.1.1-SNAPSHOT"
   :description "clojure.test integration for https://concordion.org"
   :url "https://github.com/holyjak/clj-concordion"
   :license {:name "Unlicense"
