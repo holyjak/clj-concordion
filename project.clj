a12a38601e25e9cb583448ba2a06484aec1bed1f
Jakub Holy <jakubholy@jakubholy.net>
2021-11-12 10:41:11 +0100

    Upgrade to latest Concordion 3.1

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-concordion "2.0.0-SNAPSHOT"
+(defproject clj-concordion "2.1.0"
   :description "clojure.test integration for https://concordion.org"
   :url "https://github.com/holyjak/clj-concordion"
   :license {:name "Unlicense"
             :url "https://choosealicense.com/licenses/unlicense/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  ;; Added here for IntelliJ's sake:
-                 [org.concordion/concordion "3.0.0"]
-                 [org.clojure/tools.logging "0.4.0"]]
+                 [org.concordion/concordion "3.1.3"]
+                 [org.clojure/tools.logging "1.1.0"]]
   :aot [clj-concordion.internal.interop clj-concordion.internal.run] ;; -> Java classes for Concordion interop
   :plugins [[lein-auto "0.1.3"]]
   :repl-options {:init-ns clj-concordion.core}
